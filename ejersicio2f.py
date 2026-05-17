f2e80ddfe812997db737576bb9f97cc94e306194
rwciANA <rcondorii@unsa.edu.pe>
2026-05-16 21:51:57 -0500

    implementacion 2f

---
+++ ejersicio2f.py
@@ -0,0 +1,17 @@
+from chessPictures import *
+from interpreter import draw
+
+cuadro1=square
+cuadro2=square.join(cuadro1.negative())
+cuadro3=square.join(cuadro2.negative())
+cuadro4= square.join(cuadro3.negative())
+
+cuadro5=square.join(cuadro4.negative())
+
+cuadro6=square.join(cuadro5.negative())
+cuadro7=square.join(cuadro6.negative())
+cuadro8=square.join(cuadro7.negative())
+fila1=cuadro8
+fila2= cuadro8.negative()
+filaFinal=fila1.under(fila2.under(fila1.under(fila2)))
+draw(filaFinal)
