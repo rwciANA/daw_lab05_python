71500d2de129331aabbe558f3cf75a2f00fef16e
rwciANA <rcondorii@unsa.edu.pe>
2026-05-16 21:07:33 -0500

    implementacion de ejercisio2b.py

---
+++ ejersicio2b.py
@@ -0,0 +1,9 @@
+from chessPictures import *
+from interpreter import draw
+
+fila1 = knight.join(knight.negative())
+fila2 = knight.negative().join(knight)
+
+tablero = fila1.up(fila2)
+tableroInv=tablero.negative()
+draw(tableroInv)
